065e74b15d4956fdeb085baa63b4d95b798706a9
soluluke <l.reiter@soluvine.com>
2023-08-07 14:56:28 +0200

    Doc update

--- js/hmXtopic.js
+++ js/hmXtopic.js
@@ -6,7 +6,7 @@ hmProjectInfo = {
 	copyright: "(c) Soluvine GmbH",
 	summary: "&nbsp;",
 	version: "1.0.0",
-	date: "Donnerstag, 22. Juni 2023",
+	date: "Montag, 7. August 2023",
 	language: "de-de",
 	mainfile: "index.html"
 	}
